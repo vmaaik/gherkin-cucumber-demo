8d43e1f6abe071bf42f752a16e840a87859fa5a9
michal.gebarowski <vmaaik@gmail.com>
2018-08-15 22:02:06 +0100

    solve path issue; moved feature file;

--- src/test/java/steps/Login.java
+++ src/test/java/steps/Login.java
@@ -12,45 +12,45 @@ import org.openqa.selenium.chrome.ChromeDriver;
 import java.util.concurrent.TimeUnit;
 
 public class Login {
-    private WebDriver driver;
+
 
     @Before
     public void setup() {
-        System.setProperty("webdriver.chrome.drive", "/Users/michal.gebarowski/Documents/GitProject/gherkin-cucumber-demo/src/test/resources/drivers/chromedriver");
-        this.driver = new ChromeDriver();
-        this.driver.manage().window().maximize();
-        this.driver.manage().timeouts().pageLoadTimeout(60, TimeUnit.SECONDS);
-        this.driver.manage().timeouts().setScriptTimeout(60, TimeUnit.SECONDS);
+        System.setProperty("webdriver.chrome.driver", "src/test/resources/drivers/chromedriver");
+        WebDriver driver = new ChromeDriver();
+        driver.manage().window().maximize();
+        driver.manage().timeouts().pageLoadTimeout(60, TimeUnit.SECONDS);
+        driver.manage().timeouts().setScriptTimeout(60, TimeUnit.SECONDS);
     }
 
     @Given("^User navigates to stackoverflow website$")
     public void user_navigates_to_stackoverflow_website() throws Exception {
-
+        System.out.println("test");
     }
 
     @And("^Click on the login buton on the homepage$")
     public void click_on_the_login_buton_on_the_homepage() throws Exception {
-
+        System.out.println("test");
     }
 
     @And("^User enters a valid username$")
     public void user_enters_a_valid_username() throws Exception {
-
+        System.out.println("test");
     }
 
     @And("^User enters a valid password$")
     public void user_enters_a_valid_password() throws Exception {
-
+        System.out.println("test");
     }
 
     @When("^User click on login button$")
     public void user_click_on_login_button() throws Exception {
-
+        System.out.println("test");
     }
 
     @Then("^User should be taken to the successful login page$")
     public void user_should_be_taken_to_the_successful_login_page() throws Exception {
-
+        System.out.println("test");
     }
 
 }
